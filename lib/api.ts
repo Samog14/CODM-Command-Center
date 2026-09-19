89368668e57d600f1a5497c2ab9a7a9908f9c0ff
tedph <theooye7@gmail.com>
2026-09-19 18:49:05 +0100

    fix for BE api

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 import type { ApiEnvelope, ApiErrorBody } from "./types";
 
-export const API_URL = `${process.env.API_URL}/api`;
+export const API_URL = `${process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:4000"}/api`;
 
 let authToken: string | null = null;
 
